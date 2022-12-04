50e3881ba3487fabc807a615c6e5dfd98236dc84
Arocyn <49655195+Arocyn@users.noreply.github.com>
2022-12-04 23:56:51 +0300

    Add files via upload

---
+++ Ders_1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+
+    /*
+    Bu iki işaret arasında satır satır yorum yapabiliriz.
+    */
+
+    printf("C ogrenmek cok guzel. \n");
+
+    /*
+
+    "\" ile kaçış dizisi oluştururuz.
+    "\n" ile yeni satıra geçeriz.
+    "\t" yatay sekme.
+    "\\" ters slaşı metinde gösterir.
+    "\'" tek tırnağı metinde gösterir.
+    "\"" çift tırnağı metinde gösterir.
+    "\a" uyarı sesi çıkarır.
+    "\b" geri tuşu.
+    "\e" kaçış karakteri.
+    "\f" Form besleme sayfa sonu.
+    "\r" satır başı.
+    "\v" dikey sekme.
+
+    */
+
+    printf("C kolay bir dil.\n\n");
+
+    printf("Sen ne kadar sevdin bu dili?\n");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n\t0\n\n");
+    printf("Bir sey demene gerek yok 10/10 bir dil oldugunu herkes bilir.\n");
+
+system("pause");
+
+return 0;
+}
