2d4e90e58761cac4470adb603758d0a79dddc75f
Arocyn <49655195+Arocyn@users.noreply.github.com>
2022-12-06 16:18:44 +0300

    Add files via upload

---
+++ Ders_2_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    // constant = Derleyiciye belirli bir deðiþkenin deðerinin deðiþtirilmemesi gerektiðinin bildirmesini saðlar
+
+    const float pi = 3.141519;
+
+    printf("%f",pi);
+
+    return 0;
+
+}
