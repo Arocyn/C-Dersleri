42ed70b406bd676fe572c1de5535bae6206040d7
Arocyn <49655195+Arocyn@users.noreply.github.com>
2022-12-06 16:01:40 +0300

    Add files via upload

---
+++ Ders_2_Ornek.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char isim[] = "";
+    printf("Merhaba ismizi ogrenebilir miyim? \n");
+    scanf("%s", isim);
+
+    printf("Merhaba %s , hosgeldiniz.\n", isim);
+
+    system("pause");
+
+}
